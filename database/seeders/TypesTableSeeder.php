63df880f7dcbde9e6c8cec014874b86993531fd5
AndreaFumagalli4 <98.fumagalli.andrea@gmail.com>
2023-02-28 15:25:33 +0100

    added types seeder

---
+++ database/seeders/TypesTableSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Type;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+
+class TypesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(Faker $faker)
+    {
+        for ($i=0; $i < 20; $i++) { 
+            $newType = new Type();
+            $newType->name = $faker->randomElement(['Front-end', 'Back-end', 'Full Stack']);
+            $newType->save();
+        }
+    }
+}
